6782c856e05ed71987843d84518cba7163869e0c
Alec Brooks <alec.e.brooks@gmail.com>
2013-04-18 19:10:51 +1000

    initial

---
+++ low_level.h
@@ -0,0 +1,13 @@
+void FollowLineTilEnd(void);
+
+bool DetectCan(void);
+
+int CountNodeEdges(void);
+
+bool isDark(void);
+
+void calibrateCompass(void);
+
+int currentDirection(void);
+
+bool inSafetyZone(int centre, int threshold);
